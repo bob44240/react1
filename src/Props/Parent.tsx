80b5863d0842762d4fa8eee1fe7ec501fd7896c2
Bob <bob@frieden.org>
2021-06-26 10:51:06 -0400

    First commit

---
+++ src/Props/Parent.tsx
@@ -0,0 +1,5 @@
+import {Child } from './child'; 
+const Parent = () => {
+    return <Child/>
+};
+export default Parent;
---
+++ src/Props/child.tsx
@@ -0,0 +1,3 @@
+export const Child =() => {
+    return <div> I'm a child </div>
+}
